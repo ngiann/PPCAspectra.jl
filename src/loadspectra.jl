305b3e930254a9dc5c8e2c521071bab5edea8adb
ngiann <ngiann@gmail.com>
2023-08-04 14:45:47 +0200

    allow user to get original wavelengths, docstring

--- src/loadspectra.jl
+++ src/loadspectra.jl
@@ -1,4 +1,16 @@
-function loadspectra(;N=7506, bnd=1)
+"""
+    X, Y = loadspectra(;N=7506, bnd=NaN)
+
+Load data from "A Spectral Model for Multimodal Redshift Estimation".
+Argument N controls the number of spectra returned with N=7506 being the total available number.
+If argumment bnd is specified, then the wavelengths will be scaled with in [-bnd, bnd]
+
+Returns
+
+- X is an array of arrays of wavelengths
+- Y is an array of arrays of fluxes
+"""
+function loadspectra(;N=7506, bnd=NaN)
 
     @printf("Returning %d number of spectra of dimension 1000\n", N)
     
@@ -8,12 +20,25 @@ function loadspectra(;N=7506, bnd=1)
 
     notinf(x) = isinf(x) == false
 
-    
-    x = collect(LinRange(-abs(bnd), abs(bnd), 1000))
-    
-    X = [x[findall(notinf.(A[n,:]))] for n in 1:N]
+    if ~isnan(bnd)
 
-    @printf("\t Wavelengths are in arbitrary units constrained on common grid between [%f,%f]\n",-abs(bnd),abs(bnd))
+        x = collect(LinRange(-abs(bnd), abs(bnd), 1000))
+        
+        X = [x[findall(notinf.(A[n,:]))] for n in 1:N]
+
+        @printf("\t Wavelengths are in arbitrary units constrained on common grid between [%f,%f]\n",-abs(bnd),abs(bnd))
+
+
+    else
+
+        x = collect(LinRange(500.0, 10_400.0, 1000))
+        
+        X = [x[findall(notinf.(A[n,:]))] for n in 1:N]
+
+        @printf("\t Wavelengths expressed in regular grid [500, 10400] of 1000 steps\n")
+
+    end
+
 
     Y = [max.(filter(notinf, A[n,:]), 0.0) for n in 1:N]
 
